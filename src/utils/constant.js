ec303e1f160b8e77353d647ac939ac14fa456ce5
Mayank kumar <mayank30092003@gmail.com>
2024-02-11 20:30:23 +0530

    api fetching done

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,10 +3,12 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const USER_AVATAR = "https://occ-0-3213-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
 
 
-export  const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDM0Yjg2ZTY2MjA3NDI3ZTc5ZjYwNmY1MzAxMWJmMyIsInN1YiI6IjY1YmMwNjhiYzE0NGRkMDE2M2ZlN2YyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0-oZO5lHJrixTvaQN44KGwWIAUiZwDRm7apsO3orqfo'
-    }
-  };
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDM0Yjg2ZTY2MjA3NDI3ZTc5ZjYwNmY1MzAxMWJmMyIsInN1YiI6IjY1YmMwNjhiYzE0NGRkMDE2M2ZlN2YyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0-oZO5lHJrixTvaQN44KGwWIAUiZwDRm7apsO3orqfo'
+  }
+};
+
+export const  IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
