211262f1ef42392313f835c74a35f9e0b56a0a69
Mayank kumar <mayank30092003@gmail.com>
2025-03-03 18:03:34 +0530

    updated the firebase .env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,3 +18,12 @@ export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/9134db96-10d6-
 export const SUPPORTED_LANGUAGES = [{ identifier: "en", name: "English" }, { identifier: "hindi", name: "hindi" }, { identifier: "spanish", name: "spanish" }]
 
 export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,24 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import {getAuth} from "firebase/auth";
+import {FIREBASE_CONFIG} from "./constants";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB1FS0kqIHRDmuli6hrztlEXqtMRWbE3Os",
-  authDomain: "gpt-on-flix.firebaseapp.com",
-  projectId: "gpt-on-flix",
-  storageBucket: "gpt-on-flix.appspot.com",
-  messagingSenderId: "738571551123",
-  appId: "1:738571551123:web:3a1992a69ebf520ff4e866",
-  measurementId: "G-9870MT9H45"
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
 const analytics = getAnalytics(app);
 
 export const auth = getAuth();
